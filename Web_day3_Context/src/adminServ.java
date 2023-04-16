b6a060e4f2bf7b306aae8728b0bfe010e665d161
Rishabh Umredkar <umredkarrishabh@gmail.com>
2023-04-16 16:25:34 +0530

    t2

---
+++ Web_day3_Context/src/adminServ.java
@@ -0,0 +1,47 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class adminServ extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+<<<<<<< HEAD
+			
+			
+			ServletConfig config = getServletConfig();
+			String name = config.getInitParameter("ram");	
+=======
+	
+			
+			
+			ServletContext context = getServletContext();
+			String name = context.getInitParameter("ram");
+			
+>>>>>>> 2918866 (first commit)
+			
+			out.print("Hello "+name);
+			
+			if(name.equals("Rishabh"))
+			{
+				RequestDispatcher dispatcher = request.getRequestDispatcher("reg");
+				dispatcher.forward(request, response);
+			}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
